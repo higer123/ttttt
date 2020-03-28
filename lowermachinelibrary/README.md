49214104ea89c7d3f06a1c4cea477355f39a50af
Higer-CustomLearn <liuben@cnhiger.com>
2020-03-28 17:23:30 +0800

    11

---
+++ lowermachinelibrary/README.md
@@ -0,0 +1,2 @@
+# Lowermachinelibrary
+dd
---
+++ lowermachinelibrary/src/main/java/com/higer/lowermachinelibrary/Utils.java
@@ -0,0 +1,10 @@
+package com.higer.lowermachinelibrary;
+
+import androidx.annotation.NonNull;
+
+public class Utils {
+   public static int add(int x,int y)
+   {
+       return x+y;
+   }
+}
